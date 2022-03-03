e1a487c61662504acd40777150449ae8d13ab45f
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2022-03-03 11:07:53 -0600

    Update
    Se inicializó la interfaz gráfica propuesta para probar si se puede utilizar, es la librería GIO.

--- main.go
+++ main.go
@@ -5,6 +5,19 @@ import (
 	"OLC2/interfaces"
 	"OLC2/parser"
 
+	"image/color"
+	"log"
+
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/widget/material"
+
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 )
 
@@ -29,7 +42,6 @@ func (this *TreeShapeListener) ExitStart(ctx *parser.StartContext) {
 }
 
 func main() {
-
 	// Setup the input
 	//is := antlr.NewInputStream("\"El resultado es: \" + (10+5+5+5+5+5)")
 
@@ -46,4 +58,34 @@ func main() {
 
 	antlr.ParseTreeWalkerDefault.Walk(NewTreeShapeListener(), tree)
 
+	go func() {
+		w := app.NewWindow()
+		err := run(w)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+
+}
+func run(w *app.Window) error {
+	th := material.NewTheme(gofont.Collection())
+	var ops op.Ops
+	for {
+		e := <-w.Events()
+		switch e := e.(type) {
+		case system.DestroyEvent:
+			return e.Err
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e)
+			title := material.H1(th, "mi novia es bonita, ahuevoooo")
+			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
+			title.Color = maroon
+			title.Alignment = text.Middle
+			title.Layout(gtx)
+
+			e.Frame(gtx.Ops)
+		}
+	}
 }
